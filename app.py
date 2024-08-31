7ce39407140c3e4bb7e3d33733e084ecd824dd43
sigurdmurad <sigurd.murad@intercom.io>
2024-08-31 19:55:31 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# Get the instance name from environment variables
+instance_name = os.getenv('INSTANCE_NAME', 'DefaultInstance')
+
+@app.route('/')
+def hello_world():
+    return f'Hello from {instance_name}!'
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))  # Default port is 5000 if not set
+    app.run(host='0.0.0.0', port=port)
+
