fd859afb2851d8a417b9269f1c1dac129e962a94
sigurdmurad <sigurd.murad@intercom.io>
2024-09-03 09:43:59 +0100

    Plage leon

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 
 app = Flask(__name__)
 
+app.static_folder = 'static'
+
 # Get the instance name from environment variables
 instance_name = os.getenv('INSTANCE_NAME', 'DefaultInstance')
 
@@ -17,10 +19,11 @@ def hello_world():
              ||     ||
     </pre>
     """
-    return f'<h1>Hello {instance_name}!</h1>{cow_art}'
+    image_html = '<img src="/static/images/sigurd_er_best.png" alt="Sigurd er best">'
+    return f'<h1>Hei Leon!!!</h1>{cow_art}{image_html}'
 
 if __name__ == '__main__':
     print("Starting server...")
-    port = int(os.getenv('PORT', 5000))  # Default port is 5000 if not set
+    port = int(os.getenv('PORT', 5001))  # Default port is 5000 if not set
     app.run(host='0.0.0.0', port=port)
 
