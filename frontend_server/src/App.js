73c809f237ce29f618cd6d3f6add40352afd13d4
smmurad <muradsigurd@gmail.com>
2024-09-16 16:41:00 +0100

    add imports

--- frontend_server/src/App.js
+++ frontend_server/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 // ... other imports ...
 import UploadPage from './components/UploadPage';
